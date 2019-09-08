d820a9824fe5d9aa5038e819757b57569683e6df
lukassxp <lpalucas.10@gmail.com>
2019-09-07 22:06:45 -0300

    created endpoint to list available barber schedules

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,7 @@ import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -27,6 +28,7 @@ routes.put('/users', UserController.update);
 routes.put('/notifications/:id', NotificationController.update);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 routes.get('/appointments', AppointmentController.index);
 routes.get('/schedules', ScheduleController.index);
 routes.get('/notifications', NotificationController.index);
